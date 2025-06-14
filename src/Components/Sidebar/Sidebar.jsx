6914b781e34a9ce6aa56c93392ec96b2b03830d7
abdullah navin <navinfayed26@gmail.com>
2025-06-14 12:24:14 +0600

    initial setup

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { assets } from '../../assets/assets';
+
+
+const Sidebar = () => {
+    return (
+        <div>
+            <div>
+                <img src={assets.menu_icon} alt="" />
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Sidebar;
